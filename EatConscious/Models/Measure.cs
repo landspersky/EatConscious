cccdcc037303a80984a2767504c872dd64813456
landspersky <jakub.landspersky@gmail.com>
2023-07-24 13:22:45 +0200

    Add measure infrastructure

---
+++ EatConscious/Models/Measure.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace EatConscious.Models;
+
+public abstract class Measure
+{
+    public double Value { get; init; }
+
+    protected abstract string MeasureId { get; }
+
+    public static double operator /(Measure a, Measure b)
+    {
+        Type aType = a.GetType();
+        Type bType = b.GetType();
+        if (aType != bType)
+        {
+            throw new ArgumentException($"{aType} is incompatible with {bType}");
+        }
+        return a.Value / b.Value;
+    }
+
+    public override string ToString()
+    {
+        return $"{Value} {MeasureId}";
+    }
+}
+
+public class Mililiter : Measure
+{
+    protected override string MeasureId => "ml";
+}
+
+public class Gram : Measure
+{
+    protected override string MeasureId => "g";
+}
+
+public class Piece : Measure
+{
+    protected override string MeasureId => "pcs";
+}
+
+public static class IntExtensions
+{
+    public static T ToMeasure<T>(this int x) where T:Measure, new()
+    {
+        return new T() { Value = x };
+    }
+}
--- EatConscious/ViewModels/MainWindowViewModel.cs
+++ EatConscious/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace EatConscious.ViewModels;
+﻿using System.Collections.Generic;
+using EatConscious.Models;
+using ReactiveUI;
+
+namespace EatConscious.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public List<Measure> Measures { get; set; } = new()
+    {
+        100.ToMeasure<Gram>(),
+        40.ToMeasure<Mililiter>(),
+        3.ToMeasure<Piece>()
+    };
+
+    public void Click()
+    {
+        Measures.Add(300.ToMeasure<Mililiter>());
+        //this.RaisePropertyChanged(nameof(Measures));
+    }
+    
 }

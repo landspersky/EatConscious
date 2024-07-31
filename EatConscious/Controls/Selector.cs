47816b7691ba03e6e9680df4b6cb4f17cbba609f
landspersky <jakub.landspersky@gmail.com>
2024-07-31 23:23:02 +0200

    Add possibility to create multi-/single-select controls

--- EatConscious/Controls/Selector.cs
+++ EatConscious/Controls/Selector.cs
@@ -1,9 +1,33 @@
+using System;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Data;
+using Avalonia.Data.Converters;
 
 namespace EatConscious.Controls;
+
+public class BoolSelectionModeConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool singleSelect)
+        {
+            return singleSelect
+                ? SelectionMode.Single | SelectionMode.Toggle
+                : SelectionMode.Multiple | SelectionMode.Toggle;
+        }
+        return new BindingNotification(new InvalidCastException(),
+            BindingErrorType.Error);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
 
 public class Selector : TemplatedControl
 {
@@ -33,5 +57,19 @@ public class Selector : TemplatedControl
     {
         get => _selectedItems;
         set => SetAndRaise(SelectedItemsProperty, ref _selectedItems, value);
+    }
+
+    public static readonly DirectProperty<Selector, bool> SingleSelectProperty =
+        AvaloniaProperty.RegisterDirect<Selector, bool>(
+            nameof(SingleSelect),
+            o => o.SingleSelect,
+            (o, v) => o.SingleSelect = v,
+            defaultBindingMode: BindingMode.OneWay);
+    
+    private bool _singleSelect;
+    public bool SingleSelect
+    {
+        get => _singleSelect;
+        set => SetAndRaise(SingleSelectProperty, ref _singleSelect, value);
     }
 }
